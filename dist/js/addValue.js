1f1930772a2ac8595809701466ff93b7356f2c2a
hidayat05-lab <kyyhidayat@gmail.comssh-keygen -t ed25519 -C kyyhidayat@gmail.com>
2023-01-17 21:41:09 +0700

    tamabah link

---
+++ dist/js/addValue.js
@@ -0,0 +1,11 @@
+let namaMasjid = sessionStorage.getItem("namaMasjid");
+let judulBerita = sessionStorage.getItem("judulBerita");
+let IsiBerita = sessionStorage.getItem("IsiBerita");
+let Penulis = sessionStorage.getItem("Penulis");
+let TanggalTerbit = sessionStorage.getItem("TanggalTerbit");
+
+console.log(namaMasjid);
+console.log(judulBerita);
+console.log(IsiBerita);
+console.log(Penulis);
+console.log(TanggalTerbit);
